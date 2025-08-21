65a268e692810175230876241b6ffbc988f09b61
Stevetatu <stevetatu3@gmail.com>
2025-08-22 00:06:38 +0300

    Middleware logic

--- axum_auth_backend/src/middleware.rs
+++ axum_auth_backend/src/middleware.rs
@@ -1,1 +1,101 @@
 // Handles authentication and role-based access control in the Axum application.
+use std::sync::Arc;
+use axum::{
+    extract::Request,
+    httt::{header, status},
+    middleware::Next,
+    response::IntoResponse,
+    Extension, 
+
+};
+
+use axum_extra::extract::cookie::CookieJar;
+use serde::{Deserialize, Serialize};
+use crate::{
+    db::UserExt,
+    error::{ErrorMessage, HttpError},
+    utils::token,
+    AppState,
+};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JWTMiddleware{
+    pub user: User,
+
+}
+
+pub async fn auth(
+    cookie_jar: CookieJar,
+    Extension(state): Extension<Arc<AppState>>,
+    mut req: Request,
+    next: Next,
+) -> Result<impl IntoResponse, HttpError> {
+
+    let cookies = cookie_jar
+    .get("token")
+    .map(|cookie| cookie.value().to_string());
+    .or_else(|| {
+        req.headers()
+            .get(header::AUTHORIZATION)
+            .and_then(|auth_header| auth_header.to_str().ok())
+            .and_then(|auth_value|{
+                if auth_value.starts_with("Bearer"){
+                    Some(auth_value[7..].to_owned())
+                } else {
+                    None
+                }
+            })
+    });
+
+    let token = cookies.ok_or_else(||{
+        HttpError::unauthorized(ErrorMessage::TokenNotProvided.to_string())
+    })?;
+
+    let token_details = 
+        match token::decode_token(token, app_state.env.jwt_secret.as_bytes()){
+            OK(token_details) => token_details,
+            Err(_) => {
+                return Err(HttpError::unauthorized(ErrorMessage::InvalidToken.to_string()));
+            }
+        };
+
+    let user_id = uuid::Uuid::parse_str(&token_details.to_string())
+        .map_err(|_|{
+            HttpError::unauthorized(ErrorMessage::InvalidToken.to_string())
+        })?;
+
+    let user = app_state.db_client.get_user(Some(user_id), name:None, email:None)
+        .await
+        .map_err(|_|{
+            HttpError::unauthorized(ErrorMessage::UserNolongerExist.to_strng())
+        })?;
+        
+    let user = user.ok_or_else(||{
+        HttpError::unauthorized(ErrorMessage::UserNolongerExist.to_string())
+    })?;    
+
+    req.extensions_mut().insert(JWTMiddleware){
+        user: user.clone(),
+    }
+
+    Ok(next.run(req).await)
+}
+
+pub async fn role_check(
+    Extension(_app_state: Extension<Arc<AppState>>),
+    req: Request,
+    required_roles: vec<UserRole>,
+) -> Result<impl IntoResponse, HttpError>{
+
+    let user = req
+        .extensions()
+        .get::<JWTMiddleware>()
+        .ok_or_else(||{
+            HttpError::unauthorized(ErrorMessage::UserNotAuthenticated.to_string(), status)
+        })?;
+    
+    if !required_roles.contains(&user.user.role){
+        return Err(HttpErrora::new(ErrorMessage::PermissionDenied.to_string(), status: StatusCode::FORBIDDEN));
+    }
+    Ok(next.run(req).await)
+}
